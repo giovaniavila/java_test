e9d610dc55cce9ea95cd00dccc7ca5afda4bac51
giovaniavila <giovanicarvalhoavila@hotmail.com>
2023-03-01 08:11:39 -0300

    :sparkles: java GUI

---
+++ src/ex.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+
+public class ex {
+    public static void main(String[] args){
+        JOptionPane.showMessageDialog(null, "teste de interface",
+        "Java merda", 1);
+    }
+}
