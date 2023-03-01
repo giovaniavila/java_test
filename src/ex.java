9858d4ec7e6182bd6c0f4097b04898b9e55942bb
giovaniavila <giovanicarvalhoavila@hotmail.com>
2023-03-01 08:28:33 -0300

    java GUI input and message

--- src/ex.java
+++ src/ex.java
@@ -4,5 +4,10 @@ public class ex {
     public static void main(String[] args){
         String n1=JOptionPane.showInputDialog("digite um número:");
         int numero = Integer.parseInt(n1);
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("número: ");
+        sb.append(numero);
+        JOptionPane.showMessageDialog(null, sb.toString());
     }
 }
