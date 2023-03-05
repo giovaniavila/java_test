27c5f05e4231d446fb4a2381b057c54d61701655
giovaniavila <giovanicarvalhoavila@hotmail.com>
2023-03-05 19:55:12 -0300

    :sparkles: conditionals

---
+++ src/conditionals.java
@@ -0,0 +1,9 @@
+public class conditionals {
+  public static void main(String[] args){
+    int idade = 12;
+    if(idade >= 15)
+      System.out.println("pode entrar");
+    else
+      System.out.println("não pode entrar");
+  }
+}
