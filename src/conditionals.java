348da003a2320092746a9535c04d797fa9941298
giovaniavila <giovanicarvalhoavila@hotmail.com>
2023-03-13 16:38:54 -0300

    :fire: fx and some java stdy removed

--- src/conditionals.java
+++
@@ -1,9 +0,0 @@
-public class conditionals {
-  public static void main(String[] args){
-    int idade = 12;
-    if(idade >= 15)
-      System.out.println("pode entrar");
-    else
-      System.out.println("não pode entrar");
-  }
-}
