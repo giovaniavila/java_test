6e2af0525693b58872caf6c2a3d009a6db6b2033
giovani <giovanicarvalhoavila@hotmail.com>
2023-03-28 17:24:49 -0300

    :books: :orange_book: instancias e constructor

---
+++ src/CFB/instancia/course.java
@@ -0,0 +1,10 @@
+package CFB.instancia;
+
+public class course {
+    public static void main(String[] args){
+
+        player j1 = new player(); //** new é responsável por instanciar a classe jogador, ou seja, atribui um novo objeto de outra classe associado ao j1 */
+    }
+}
+
+
---
+++ src/CFB/instancia/player.java
@@ -0,0 +1,9 @@
+package CFB.instancia;
+
+public class player {
+    public player(){ //** o método construtor é criado ao instanciar um novo objeto e ele não tem tipo de retorno, responsável pela criação do objeto naquela classe */
+        System.out.printf("%nplayer criado%n");;
+    }
+}
+
+
