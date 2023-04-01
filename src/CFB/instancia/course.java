53c7e0392fa5d04762b783afc356b65c94a600db
giovani <giovanicarvalhoavila@hotmail.com>
2023-04-01 09:51:59 -0300

    :books: :orange_book: parâmetros de entrada

--- src/CFB/instancia/course.java
+++ src/CFB/instancia/course.java
@@ -3,7 +3,8 @@ package CFB.instancia;
 public class course {
     public static void main(String[] args){
 
-        player j1 = new player(); //** new é responsável por instanciar a classe jogador, ou seja, atribui um novo objeto de outra classe associado ao j1 */
+        player j1 = new player(1); //** new é responsável por instanciar a classe jogador, ou seja, atribui um novo objeto de outra classe associado ao j1 */
+        player j2 = new player(2);
     }
 }
 
--- src/CFB/instancia/player.java
+++ src/CFB/instancia/player.java
@@ -1,8 +1,8 @@
 package CFB.instancia;
 
 public class player {
-    public player(){ //** o método construtor é criado ao instanciar um novo objeto e ele não tem tipo de retorno, responsável pela criação do objeto naquela classe */
-        System.out.printf("%nplayer criado%n");;
+    public player(int n){ //** o método construtor é criado ao instanciar um novo objeto e ele não tem tipo de retorno, responsável pela criação do objeto naquela classe */
+        System.out.printf("%nplayer numero %d criado%n", n);; //* %d mostra o parâmetro de entrada */
     }
 }
 
